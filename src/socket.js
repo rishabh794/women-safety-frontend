d0dc880861dec82b860aeff05a9022f5a0440d38
rishabh794 <rishabhbuchha123456789@gmail.com>
2025-10-01 19:00:12 +0530

    change url for socket

--- src/socket.js
+++ src/socket.js
@@ -1,4 +1,7 @@
 import { io } from "socket.io-client";
 
-const URL = "http://localhost:3000";
-export const socket = io(URL);
+const URL = import.meta.env.VITE_API_BASE_URL;
+
+export const socket = io(URL, {
+  autoConnect: false,
+});
