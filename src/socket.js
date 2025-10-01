e638e84405bb92d6a50a38523b72d45124017b13
rishabh794 <rishabhbuchha123456789@gmail.com>
2025-10-01 22:12:07 +0530

    fix socket url encoding error

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,8 @@
 import { io } from "socket.io-client";
 
-const URL = import.meta.env.VITE_API_BASE_URL;
+const URL = import.meta.env.VITE_SOCKET_URL;
 
 export const socket = io(URL, {
   autoConnect: false,
+  transports: ["websocket"],
 });
